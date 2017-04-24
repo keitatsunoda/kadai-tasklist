12e2e9ea8f6ea733e3b046b7b8b14c622006ac04
Keita Tsunoda <tsunokeikei@gmail.com>
2017-04-24 00:09:51 +0000

    toppage navbar fixed

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
     
     # root to: 'tasks#index'
     root to: 'toppages#index'
-    resources :tasks
+    get 'signup' , to: 'users#new'
+    resources :users, only: [:index,:show, :create, :create]
     
     
 end
